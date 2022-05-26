a3c4c1832973f860771d4f5b6d2087789da36416
Saulo R <rvro.saulo@gmail.com>
2022-05-26 00:59:21 -0300

    final condicionales js

--- Javascript/2/app.js
+++ Javascript/2/app.js
@@ -1,7 +1,17 @@
-console.log("Antes del Condicional")
+// console.log("Antes del Condicional")
 
-if (1+1===2) {
-    console.log ("La matematica funciona")
-}
+// if (1+1===2) {
+//     console.log ("La matematica funciona")
+// }
 
-console.log("Despues del condicional")
+// console.log("Despues del condicional")
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log ("Odio los Lunes!")
+// } else if (dayOfWeek === 'Saturday') {
+//     console.log ("Amo los Sabados!")
+// } else if (dayOfWeek === 'Friday') {
+//     console.log("Ta Bueno")
+// }
