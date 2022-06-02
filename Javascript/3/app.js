61416a8aa7e1f69acd1fdf477cf2d3dc78bf4520
Saulo R <rvro.saulo@gmail.com>
2022-06-01 22:28:41 -0300

    Object Access Exercise

--- Javascript/3/app.js
+++ Javascript/3/app.js
@@ -7,6 +7,5 @@ const restaurant = {
     zipcode: '11206',
 }
 
-console.log("Funciona")
 //YOUR CODE GOES DOWN HERE:
-// let fullAddress = restaurant.
+let fullAddress = restaurant.address +', '+ restaurant.city+', '+restaurant.state+ ', '+restaurant.zipcode
