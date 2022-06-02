f0fdf9841691af6fffec70ea11359bb0518f3497
Saulo R <rvro.saulo@gmail.com>
2022-06-02 02:31:30 -0300

    iterating array exercise

--- Javascript/4/app.js
+++ Javascript/4/app.js
@@ -4,7 +4,22 @@ console.log ("funciona")
 //     console.log(num);
 // }
 
-for (let num=1; num<=6; num++) {
-    console.log("Da ba dee da ba daa");
-    (num);
+// for (let num=1; num<=6; num++) {
+//     console.log("Da ba dee da ba daa");
+//     (num);
+// }
+
+
+// bucle para numeros pares
+for (let i=0; i<=20; i+=2) {
+    console.log(i);
+}
+
+// cuenta regresiva
+// for (let i = 100; i>=0; i -=10) {
+//     console.log(i);
+// }
+
+for (let i=25; i>=0; i -=5) {
+    console.log(i);
 }
