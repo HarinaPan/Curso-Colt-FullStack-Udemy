756306da9a01d1104c532af7840a1a0bdc16b43d
Saulo R <rvro.saulo@gmail.com>
2022-06-02 01:34:39 -0300

    first loop practice

---
+++ Javascript/4/app.js
@@ -0,0 +1,10 @@
+console.log ("funciona")
+
+// for (let num=1; num<=10;num++) {
+//     console.log(num);
+// }
+
+for (let num=1; num<=6; num++) {
+    console.log("Da ba dee da ba daa");
+    (num);
+}
