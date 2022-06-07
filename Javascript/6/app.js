1bc40c2de7f5c3b3db06a721fcd1c1fa7c2d746d
Saulo R <rvro.saulo@gmail.com>
2022-06-06 22:38:38 -0300

    Terminando el guessing game

---
+++ Javascript/6/app.js
@@ -0,0 +1,24 @@
+let maximun = parseInt(prompt("Enter a Maximun Number!"));
+while  (!maximun) {
+    maximun = parseInt(prompt("Enter a valid number!"));
+}
+
+
+const targetNum = Math.floor(Math.random()* maximun) +1;
+console.log(targetNum)
+
+let guess = parseInt(prompt("Enter your first guess!"));
+let attempts =1;
+
+while (guess !== targetNum ) {
+    attempts++;
+    if (guess > targetNum) {
+        guess = parseInt(prompt("Too high! enter a new guess:"));
+    } else {
+        guess = parseInt(prompt("Too Low! enter a new guess:"));
+    }
+
+
+}
+
+console.log("You got it in "+attempts+" attempts");
