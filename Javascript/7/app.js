b08c2f1e73dcef38c0b398b0e2e4122e3c878003
Saulo R <rvro.saulo@gmail.com>
2022-06-15 17:29:44 -0300

    Function Expression Exercise

--- Javascript/7/app.js
+++ Javascript/7/app.js
@@ -26,3 +26,35 @@ function sumArray (array) {
 
 sumArray([-10,-5,5,10]);
 console.log(a);
+
+function returnDay (numero) {
+    if (numero===1) {
+        console.log("lunes")
+        return "Monday"
+    } else if (numero===2) {
+        return "Tuesday"
+    } else if (numero===3) {
+        return "Wednesday"
+    } else if (numero===4) {
+        return "Thursday"
+    } else if (numero===5) {
+        return "Friday"
+    } else if (numero===6) {
+        return "Saturday"
+    } else if (numero===7) {
+        return "Sunday"
+    } else {
+        console.log("null")
+        return null;
+    }
+}
+
+returnDay (1);
+
+console.log ("fin")
+
+console.log(Math.sqrt(9))
+
+const square = function (numer) {
+    return Math.sqrt(numer);
+}
