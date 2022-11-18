507170e78008a8a81fb1676b02d17a4b77ce83ad
Saulo R <rvro.saulo@gmail.com>
2022-11-18 03:57:15 -0300

    terminando repaso intro funciones

--- Javascript/Back Again/Functions/app.js
+++ Javascript/Back Again/Functions/app.js
@@ -5,3 +5,27 @@ console.log("Do");
 console.log("Re");
 console.log("Mi");
 }
+
+
+function multiply (num1,num2) {
+    return (num1*num2);
+}
+
+function isShortsWeather (temperature) {
+    if (temperature>=75) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function lastElement (array) {
+    if (array.lenght<=0) {
+        return null;
+    } else {
+        let b=array.lenght-1;
+        return  array[b];
+    }
+}
+
+
